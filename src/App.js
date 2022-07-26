cc14f6db64716b385d1f0da17a0703d82c661c8c
Leandro Gavioli <leandro.gavioli@yahoo.com.br>
2022-07-26 18:56:29 -0300

    Requisito 4

--- src/App.js
+++ src/App.js
@@ -1,13 +1,62 @@
 import React from 'react';
-
 import Form from './components/Form';
+import Card from './components/Card';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.onInputChange = this.onInputChange.bind(this);
+    this.state = {
+      cardName: '',
+      cardDescription: '',
+      cardAttr1: '',
+      cardAttr2: '',
+      cardAttr3: '',
+      cardImage: '',
+      cardRare: '',
+      cardTrunfo: false,
+      // isSaveButtonDisabled: true,
+    };
+  }
+
+  onInputChange({ target }) {
+    const { name } = target;
+    const card = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({
+      [name]: card,
+    });
+  }
+  // this.onSaveButtonClick = this.onSaveButtonClick.bind(this);
+
   render() {
+    const {
+      cardName,
+      cardDescription,
+      cardImage,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardRare,
+      cardTrunfo,
+    // isSaveButtonDisabled,
+    } = this.state;
     return (
       <div>
         <h1>Tryunfo</h1>
-        <Form />
+        <Form
+          onInputChange={ this.onInputChange }
+          // onSaveButtonClick={ this.onSaveButtonClick }
+        />
+        <Card
+          cardName={ cardName }
+          cardDescription={ cardDescription }
+          cardImage={ cardImage }
+          cardAttr1={ cardAttr1 }
+          cardAttr2={ cardAttr2 }
+          cardAttr3={ cardAttr3 }
+          cardRare={ cardRare }
+          cardTrunfo={ cardTrunfo }
+        />
       </div>
     );
   }
